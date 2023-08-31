4eb149745d7ac374e33fa09ea19c029e3047f3a7
Jon Seager <jon@sgrs.uk>
2023-08-31 08:37:03 +0100

    test: simplify relation unit tests with new add_relation

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -84,9 +84,7 @@ class TestCharm(unittest.TestCase):
     @patch("charm.ParcaAgent.configure")
     def test_metrics_endpoint_relation(self, _):
         # Create a relation to an app named "prometheus"
-        rel_id = self.harness.add_relation("metrics-endpoint", "prometheus")
-        # Add a prometheus unit
-        self.harness.add_relation_unit(rel_id, "prometheus/0")
+        rel_id = self.harness.add_relation("metrics-endpoint", "prometheus", unit_data={})
         # Ugly re-init workaround: manually call `set_scrape_job_spec`
         # https://github.com/canonical/operator/issues/736
         self.harness.charm.metrics_endpoint_provider.set_scrape_job_spec()
@@ -102,17 +100,15 @@ class TestCharm(unittest.TestCase):
     @patch("charm.ParcaAgent.configure")
     def test_parca_external_store_relation(self, configure):
         self.harness.set_leader(True)
-        # Create a relation to an app named "polar-signals-cloud"
-        rel_id = self.harness.add_relation("parca-store-endpoint", "polar-signals-cloud")
-        # Add a polar-signals-cloud unit
-        self.harness.add_relation_unit(rel_id, "polar-signals-cloud/0")
-        # Set some data from the remote application
         store_config = {
             "remote-store-address": "grpc.polarsignals.com:443",
             "remote-store-bearer-token": "deadbeef",
             "remote-store-insecure": "false",
         }
-        self.harness.update_relation_data(rel_id, "polar-signals-cloud", store_config)
+        # Create a relation to an app named "polar-signals-cloud"
+        rel_id = self.harness.add_relation(
+            "parca-store-endpoint", "polar-signals-cloud", app_data=store_config
+        )
         # Ensure that we call the configure method on Parca with the correct store details
         configure.assert_called_with(store_config)
         configure.reset()
