a5d404fa4e9f896a02216742d9fdee29f8004f96
Pietro Pasotti <pietro.pasotti@canonical.com>
2025-02-06 16:37:18 +0100

    dual track for noble and jammy

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,12 +1,38 @@
 # Copyright 2021 Canonical Ltd.
 # See LICENSE file for licensing details.
+import logging
+from pathlib import Path
+from typing import List
 
 from pytest import fixture
 from pytest_operator.plugin import OpsTest
 
+logger = logging.getLogger(__name__)
 
 @fixture(scope="module")
-async def parca_charm(ops_test: OpsTest):
-    """Parca charm used for integration testing."""
-    charm = await ops_test.build_charm(".")
-    return charm
+async def build_charms(ops_test: OpsTest):
+    """Build the parca charms used for integration testing."""
+    await ops_test.build_charm(".", verbosity="verbose")
+    charms= list(Path('.').glob("*.charm"))
+    logger.info(f"packed {charms}")
+    return charms
+
+
+def _find_charm(built:List[str], version:str):
+    charm = [c for c in built if version in c]
+    if not charm:
+        raise FileNotFoundError(
+            f"charm for {version} not found in built charms: {built!r}"
+        )
+    return charm[0]
+
+
+@fixture(scope="module")
+def parca_charm_noble(build_charms):
+    """Parca charm with 24.04 base."""
+    return _find_charm(build_charms, "24.04")
+
+@fixture(scope="module")
+async def parca_charm_jammy(build_charms):
+    """Parca charm with 22.04 base."""
+    return _find_charm(build_charms, "22.04")
--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -7,21 +7,30 @@ import asyncio
 from pytest import mark
 from pytest_operator.plugin import OpsTest
 
-AGENT = "parca-agent"
+AGENT_JAMMY = "parca-agent-jammy"
+AGENT_NOBLE = "parca-agent-noble"
 UBUNTU = "ubuntu-lite"
 UBUNTU_APP_JAMMY = "ubuntu-jammy"
 UBUNTU_APP_NOBLE = "ubuntu-noble"
-UNIT_0 = f"{AGENT}/0"
+NOBLE_BASE = "ubuntu@24.04"
+JAMMY_BASE = "ubuntu@22.04"
 
 
 @mark.abort_on_fail
 @mark.skip_if_deployed
 @mark.setup
-async def test_deploy(ops_test: OpsTest, parca_charm):
-    await ops_test.model.deploy(parca_charm, application_name=AGENT, num_units=0)
+async def test_deploy(ops_test: OpsTest, parca_charm_noble, parca_charm_jammy):
+    await asyncio.gather(
+        ops_test.model.deploy(parca_charm_noble,
+                              application_name=AGENT_NOBLE,
+                              num_units=0),
+        ops_test.model.deploy(parca_charm_jammy,
+                              application_name=AGENT_JAMMY,
+                              num_units=0)
+    )
 
 
-async def test_agent_running(ops_test: OpsTest):
+async def test_agent_running_on_noble_with_virt(ops_test: OpsTest):
     # Deploy principal on a virtual-machine with ubuntu@24.04 for parca-agent to start.
     # check https://github.com/canonical/parca-agent-operator/issues/37
     # and https://github.com/canonical/parca-agent-operator/issues/47
@@ -29,55 +38,51 @@ async def test_agent_running(ops_test: OpsTest):
         ops_test.model.deploy(
             UBUNTU,
             application_name=UBUNTU_APP_NOBLE,
-            base="ubuntu@24.04",
+            base=NOBLE_BASE,
             constraints={"virt-type": "virtual-machine"},
         ),
         ops_test.model.wait_for_idle(apps=[UBUNTU_APP_NOBLE], status="active", timeout=1000),
     )
     async with ops_test.fast_forward():
         await asyncio.gather(
-            ops_test.model.integrate(UBUNTU_APP_NOBLE, AGENT),
+            ops_test.model.integrate(UBUNTU_APP_NOBLE, AGENT_NOBLE),
             # parca-agent will be in active/idle
             ops_test.model.wait_for_idle(
-                apps=[AGENT, UBUNTU_APP_NOBLE], status="active", timeout=500
+                apps=[AGENT_NOBLE, UBUNTU_APP_NOBLE], status="active", timeout=500
             ),
         )
 
 
-async def test_remove_relation(ops_test: OpsTest):
-    await ops_test.juju("remove-relation", AGENT, UBUNTU_APP_NOBLE)
+async def test_remove_relation_noble(ops_test: OpsTest):
+    await ops_test.juju("remove-relation", AGENT_NOBLE, UBUNTU_APP_NOBLE)
 
 
-async def test_agent_blocked(ops_test: OpsTest):
+async def test_agent_blocked_on_jammy_no_virt(ops_test: OpsTest):
     await asyncio.gather(
         ops_test.model.deploy(
             UBUNTU,
             application_name=UBUNTU_APP_JAMMY,
             channel="stable",
-            base="ubuntu@22.04",
+            base=JAMMY_BASE,
         ),
         ops_test.model.wait_for_idle(apps=[UBUNTU_APP_JAMMY], status="active", timeout=1000),
     )
     async with ops_test.fast_forward():
         await asyncio.gather(
-            ops_test.model.integrate(UBUNTU_APP_JAMMY, AGENT),
+            ops_test.model.integrate(UBUNTU_APP_JAMMY, AGENT_JAMMY),
             ops_test.model.wait_for_idle(apps=[UBUNTU_APP_JAMMY], status="active", timeout=500),
             # parca-agent will be in blocked state, as the snap failed to start due to permission errors.
-            ops_test.model.wait_for_idle(apps=[AGENT], status="blocked", timeout=500),
+            ops_test.model.wait_for_idle(apps=[AGENT_JAMMY], status="blocked", timeout=500),
         )
 
 
 @mark.teardown
-async def test_remove_agent(ops_test: OpsTest):
-    await ops_test.model.remove_application(AGENT)
+async def test_remove_relation_jammy(ops_test: OpsTest):
+    # unjam
+    await ops_test.juju("remove-relation", AGENT_JAMMY, UBUNTU_APP_JAMMY)
 
 
-# @mark.abort_on_fail
-# @retry(wait=wexp(multiplier=2, min=1, max=30), stop=stop_after_attempt(10), reraise=True)
-# async def test_application_is_up(ops_test: OpsTest):
-#     status = await ops_test.model.get_status()  # noqa: F821
-#     address = status["applications"][UBUNTU]["units"][f"{UBUNTU}/0"]["public-address"]
-#     response = requests.get(f"http://{address}:7071/")
-#     assert response.status_code == 200
-#     response = requests.get(f"http://{address}:7071/metrics")
-#     assert response.status_code == 200
+@mark.teardown
+async def test_remove_agents(ops_test: OpsTest):
+    await ops_test.model.remove_application(AGENT_JAMMY)
+    await ops_test.model.remove_application(AGENT_NOBLE)
