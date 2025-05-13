1fda8b089653a109605c4de76e51d520b963363e
michael <michael.dmitry98@gmail.com>
2025-05-13 11:46:58 +0300

    remove jammy from itests

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -5,7 +5,6 @@ import os
 import shlex
 from pathlib import Path
 from subprocess import check_call
-from typing import List
 
 from pytest import fixture
 
@@ -13,29 +12,11 @@ logger = logging.getLogger(__name__)
 
 
 @fixture(scope="module")
-async def build_charms():
+async def parca_charm():
     """Build the parca charms used for integration testing."""
     if not os.environ.get("CHARM_PATH"):  # if no charm is passed, pack it
         check_call(shlex.split("charmcraft pack -v"))
     charms = [f"./{path}" for path in Path(".").glob("*.charm")]
     logger.info(f"packed {charms}")
-    return charms
+    return charms["24.04"]
 
-
-def _find_charm(built: List[str], version: str):
-    charm = [c for c in built if version in c]
-    if not charm:
-        raise FileNotFoundError(f"charm for {version} not found in built charms: {built!r}")
-    return charm[0]
-
-
-@fixture(scope="module")
-def parca_charm_noble(build_charms):
-    """Parca charm with 24.04 base."""
-    return _find_charm(build_charms, "24.04")
-
-
-@fixture(scope="module")
-async def parca_charm_jammy(build_charms):
-    """Parca charm with 22.04 base."""
-    return _find_charm(build_charms, "22.04")
--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -7,85 +7,50 @@ import asyncio
 from pytest import mark
 from pytest_operator.plugin import OpsTest
 
-AGENT_JAMMY = "parca-agent-jammy"
-AGENT_NOBLE = "parca-agent-noble"
+PARCA_AGENT = "parca-agent"
 UBUNTU = "ubuntu-lite"
-UBUNTU_APP_JAMMY = "ubuntu-jammy"
 UBUNTU_APP_NOBLE = "ubuntu-noble"
-NOBLE_BASE = "ubuntu@24.04"
-JAMMY_BASE = "ubuntu@22.04"
 
 
 @mark.abort_on_fail
 @mark.skip_if_deployed
 @mark.setup
-async def test_deploy(ops_test: OpsTest, parca_charm_noble, parca_charm_jammy):
+async def test_deploy(ops_test: OpsTest, parca_charm):
     await asyncio.gather(
-        ops_test.model.deploy(parca_charm_noble,
-                              application_name=AGENT_NOBLE,
+        ops_test.model.deploy(parca_charm,
+                              application_name=PARCA_AGENT,
                               num_units=0),
-        ops_test.model.deploy(parca_charm_jammy,
-                              application_name=AGENT_JAMMY,
-                              num_units=0)
     )
 
 
 async def test_deploy_on_noble_with_virt(ops_test: OpsTest):
     # Deploy principal on a virtual-machine with ubuntu@24.04 for parca-agent to start.
-    # check https://github.com/canonical/parca-agent-operator/issues/37
-    # and https://github.com/canonical/parca-agent-operator/issues/47
     await asyncio.gather(
         ops_test.model.deploy(
             UBUNTU,
             application_name=UBUNTU_APP_NOBLE,
-            base=NOBLE_BASE,
+            base="24.04",
             constraints={"virt-type": "virtual-machine"},
         ),
         ops_test.model.wait_for_idle(apps=[UBUNTU_APP_NOBLE], status="active", timeout=1000),
     )
     async with ops_test.fast_forward():
         await asyncio.gather(
-            ops_test.model.integrate(UBUNTU_APP_NOBLE, AGENT_NOBLE),
+            ops_test.model.integrate(UBUNTU_APP_NOBLE, PARCA_AGENT),
             ops_test.model.wait_for_idle(
                 apps=[UBUNTU_APP_NOBLE], status="active", timeout=500
             ),
             # parca-agent will be in blocked because there's no remote store backend configured
             ops_test.model.wait_for_idle(
-                apps=[AGENT_NOBLE], status="blocked", timeout=500
+                apps=[PARCA_AGENT], status="blocked", timeout=500
             )
         )
 
 
 async def test_remove_relation_noble(ops_test: OpsTest):
-    await ops_test.juju("remove-relation", AGENT_NOBLE, UBUNTU_APP_NOBLE)
-
-
-async def test_deploy_on_jammy_no_virt(ops_test: OpsTest):
-    await asyncio.gather(
-        ops_test.model.deploy(
-            UBUNTU,
-            application_name=UBUNTU_APP_JAMMY,
-            channel="stable",
-            base=JAMMY_BASE,
-        ),
-        ops_test.model.wait_for_idle(apps=[UBUNTU_APP_JAMMY], status="active", timeout=1000),
-    )
-    async with ops_test.fast_forward():
-        await asyncio.gather(
-            ops_test.model.integrate(UBUNTU_APP_JAMMY, AGENT_JAMMY),
-            ops_test.model.wait_for_idle(apps=[UBUNTU_APP_JAMMY], status="active", timeout=500),
-            # parca-agent will be in blocked state, as the snap failed to start due to permission errors.
-            ops_test.model.wait_for_idle(apps=[AGENT_JAMMY], status="blocked", timeout=500),
-        )
-
-
-@mark.teardown
-async def test_remove_relation_jammy(ops_test: OpsTest):
-    # unjam
-    await ops_test.juju("remove-relation", AGENT_JAMMY, UBUNTU_APP_JAMMY)
+    await ops_test.juju("remove-relation", PARCA_AGENT, UBUNTU_APP_NOBLE)
 
 
 @mark.teardown
 async def test_remove_agents(ops_test: OpsTest):
-    await ops_test.model.remove_application(AGENT_JAMMY)
-    await ops_test.model.remove_application(AGENT_NOBLE)
+    await ops_test.model.remove_application(PARCA_AGENT)
